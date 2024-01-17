843c5fc9e8afb4e93ee233bbdde67593cd7821e3
Ho Yeung Chim <hychim@protonmail.com>
2024-01-17 16:57:56 +0100

    Update single contifs and dragging module

---
+++ example.sh
@@ -0,0 +1,7 @@
+./scripts/run_inference_asy.py \
+        inference.symmetry="C3" \
+	inference.asy_motif="True" \
+        inference.num_designs=1 \
+        inference.output_prefix=./output/test/test \
+        inference.input_pdb=./examples/input_pdbs/1DBB_interface_30A.pdb \
+        "contigmap.contigs=[5/Y471-485/10-30/X95-101/10-30/X246-251/10-30/X265-271/1/X273-278/10-30/X313-322/5/0]"\
